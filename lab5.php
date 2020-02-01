b69a72f69dd2fe9c26e99c3d43bff2985d8c85f9
codycmartin <codycmartin89@gmail.com>
2020-02-01 00:10:15 -0800

    Lab No. 5 Submission

--- lab5.php
+++ lab5.php
@@ -9,7 +9,10 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Lab No. 5</title>
 </head>
-<body>
+<body> 
+
+<!-- html for tree type are all in the inc folder -->
+
 <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Clark College Aboretum</a>
@@ -50,83 +53,110 @@
                     campus today and for many years to come.</p>
         </div>
     </div>
+    <?php
+    
+    // This variable is to trigger the default page view if no parameters are set
+    // key names are treetype1, treetype2, treetype3
 
-    <div class="container-fluid mt-3">
-        <h3 class="text-white p-3 shadow">Aralia</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header align-leff">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63472,-122.6500">Castor Aralia
-                        </a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/castor_aralia.jpg" alt="Kalopanax pictus">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Aralia<br>
-                            <strong>Botanical Name:</strong> <em>Kalopanax pictus</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    $default = false;
+         
+    if (isset($_GET['treetype1'])) {
+        $default = true;
+    }
+    if (isset($_GET['treetype2'])) {
+        $default = true;
+    }
+    if (isset($_GET['treetype3'])) {
+        $default = true;
+    }
 
-        <h3 class="p-3 shadow">Arborvitae</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63472,-122.65247">American
-                            Arborvitae</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/evergreen48.jpg" alt="occidentalis">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Arborvitae<br>
-                            <strong>Botanical Name:</strong> <em>occidentalis</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    // If a parameter is given, run through this if statement
 
-        <h3 class="p-3 shadow">Ash</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.635857,-122.652392">Rose Hill Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous33.jpg" alt="americana">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> <em>americana</em></p>
-                    </div>
-                </div>
-            </div>
+    if ($default == true) {
 
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63591,-122.65031">Raywood Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous34.jpg" alt="angustifolia">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> <em>angustifolia</em></p>
-                    </div>
-                </div>
-            </div>
+        // If you are in this is statement, checks for each individual key and enters switch statement 
+        // to display correct tree type
 
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.635541,-122.649899">Oregon Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous35.jpg" alt="latifolia">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> ∏<em>latifolia</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        if(isset($_GET['treetype1'])){
+        
+            switch (ucfirst($_GET['treetype1'])) {
+                case 'Aralia':
+                    include 'inc/Aralia.html';
+                    break;
+    
+                case 'Arborvitae':
+                    include 'inc/Arborvitae.html';
+                    break;
+    
+                case 'Ash':
+                    include 'inc/Ash.html';
+                    break;
+                
+                default:           
+                
+                    break;
+                }
+        }
+   
+        if(isset($_GET['treetype2'])){
+
+            switch (ucfirst($_GET['treetype2'])) {
+                case 'Aralia':
+                    include 'inc/Aralia.html';
+                    break;
+    
+                case 'Arborvitae':
+                    include 'inc/Arborvitae.html';
+                    break;
+    
+                case 'Ash':
+                    include 'inc/Ash.html';
+                    break;
+                
+                default:                
+                
+                    break;
+            }
+        }
+
+        if(isset($_GET['treetype3'])){
+
+            switch (ucfirst($_GET['treetype3'])) {
+                case 'Aralia':
+                    include 'inc/Aralia.html';
+                    break;
+    
+                case 'Arborvitae':
+                    include 'inc/Arborvitae.html';
+                    break;
+    
+                case 'Ash':
+                    include 'inc/Ash.html';
+                    break;
+                
+                default:
+                    
+                
+                    break;
+            }
+        }
+    }
+    // default page view 
+
+    if ($default == false){
+        include 'inc/Aralia.html';
+        include 'inc/Arborvitae.html';
+        include 'inc/Ash.html';
+    }
+    
+         
+
+      
+
+    ?>
+
+
+
 </div>
 
 <!-- jQuery -->
