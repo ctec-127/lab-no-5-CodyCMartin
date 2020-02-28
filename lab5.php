48b426371f5f3028f2282eaeff07fc8eacbd8af8
codycmartin <codycmartin89@gmail.com>
2020-02-28 09:38:59 -0800

    include files named properly. Containers fixed

--- lab5.php
+++ lab5.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,12 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Lab No. 5</title>
 </head>
-<body> 
 
-<!-- html for tree type are all in the inc folder -->
+<body>
 
-<div class="container-fluid">
+    <!-- html for tree type are all in the inc folder -->
+
+    <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Clark College Aboretum</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -43,16 +45,25 @@
                 device, you can search through the trees
                 growing on campus.
                 <p>The Clark College Arboretum exists because of the vision and dedication of faculty and staff members
-                    of Clark College who recognized the value of connecting education to the natural beauty of the campus landscape at 1933 Fort Vancouver Way. The Arboretum owes much of its abundance to the work and dedication of former Biology Professor Anna Pechanec. It was under her direction and shovel that many of the tree species on campus were planted in the 1950s through the 1970s. The Arboretum now boasts over 120 distinct
+                    of Clark College who recognized the value of connecting education to the natural beauty of the
+                    campus landscape at 1933 Fort Vancouver Way. The Arboretum owes much of its abundance to the work
+                    and dedication of former Biology Professor Anna Pechanec. It was under her direction and shovel that
+                    many of the tree species on campus were planted in the 1950s through the 1970s. The Arboretum now
+                    boasts over 120 distinct
                     species, including a number of cultivated varieties.</p>
                 <p>The Clark College Facilities Services Grounds Maintenance Department maintains our Arboretum.
                     Oversight of campus tree management is performed by the Campus Tree Advisory Committee according to
-                    guidelines established by the Arbor Day Foundation's Tree Campus USA Program, of which Clark College is a proud awardee. The committee serves to provide guidance on issues such as: campus tree health, opportunities to plant additional species and varieties, preserving existing trees in the face of growing needs
-                    for building expansion, as well as planning campus tree-related projects and celebrations. Our goal is
+                    guidelines established by the Arbor Day Foundation's Tree Campus USA Program, of which Clark College
+                    is a proud awardee. The committee serves to provide guidance on issues such as: campus tree health,
+                    opportunities to plant additional species and varieties, preserving existing trees in the face of
+                    growing needs
+                    for building expansion, as well as planning campus tree-related projects and celebrations. Our goal
+                    is
                     to preserve and expand the Clark College Arboretum for the benefit of all who find themselves on the
                     campus today and for many years to come.</p>
         </div>
     </div>
+
     <?php
     
     // This variable is to trigger the default page view if no parameters are set
@@ -81,15 +92,15 @@
         
             switch (ucfirst($_GET['treetype1'])) {
                 case 'Aralia':
-                    include 'inc/Aralia.html';
+                    include 'inc/aralia.inc.html';
                     break;
     
                 case 'Arborvitae':
-                    include 'inc/Arborvitae.html';
+                    include 'inc/arborvitae.inc.html';
                     break;
     
                 case 'Ash':
-                    include 'inc/Ash.html';
+                    include 'inc/ash.inc.html';
                     break;
                 
                 default:           
@@ -102,15 +113,15 @@
 
             switch (ucfirst($_GET['treetype2'])) {
                 case 'Aralia':
-                    include 'inc/Aralia.html';
+                    include 'inc/aralia.inc.html';
                     break;
     
                 case 'Arborvitae':
-                    include 'inc/Arborvitae.html';
+                    include 'inc/arborvitae.inc.html';
                     break;
     
                 case 'Ash':
-                    include 'inc/Ash.html';
+                    include 'inc/ash.inc.html';
                     break;
                 
                 default:                
@@ -123,15 +134,15 @@
 
             switch (ucfirst($_GET['treetype3'])) {
                 case 'Aralia':
-                    include 'inc/Aralia.html';
+                    include 'inc/aralia.inc.html';
                     break;
     
                 case 'Arborvitae':
-                    include 'inc/Arborvitae.html';
+                    include 'inc/arborvitae.inc.html';
                     break;
     
                 case 'Ash':
-                    include 'inc/Ash.html';
+                    include 'inc/ash.inc.html';
                     break;
                 
                 default:
@@ -144,24 +155,25 @@
     // default page view 
 
     if ($default == false){
-        include 'inc/Aralia.html';
-        include 'inc/Arborvitae.html';
-        include 'inc/Ash.html';
+        include 'inc/aralia.inc.html';
+        include 'inc/arborvitae.inc.html';
+        include 'inc/ash.inc.html';
     }
     
          
 
       
 
-    ?>
+?>
 
 
 
-</div>
 
-<!-- jQuery -->
-<script src="js/jquery-3.3.1.min.js"></script>
-<!-- Bootstrap JavaScript -->
-<script src="js/bootstrap.min.js"></script>
+
+    <!-- jQuery -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
